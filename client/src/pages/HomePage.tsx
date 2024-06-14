710d1e60ec05a4c510a082b4c7a86477ccec0251
HarudaySharma <harudayvasist@gmail.com>
2024-06-14 12:57:34 +0530

    feat: Home Page UI

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,23 +1,158 @@
 import { useNavigate } from "react-router";
 import { Button } from "../@/components/ui/button"
+import McqImage from "../assets/mcq-image.png"
+import Logo from "../assets/logo.png"
 
 const HomePage = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="grid max-w-full bg-sky-50 mx-auto">
-            <h1 className="text-3xl text-center font-bold">
-                WELCOME
-            </h1>
-            <div className="flex flex-col gap-4">
-                <Button onClick={() => navigate('/quiz')} className="p-16 px-24 mx-4">
-                    Take a Quiz
-                </Button>
-                <Button onClick={() => navigate('/quiz/test')} className="p-16 px-24 mx-4">
-                    Take a MCQ Test
-                </Button>
-            </div>
-        </div >
+        <div>
+            <img
+                src={Logo}
+                className="
+                    top-2                            
+                    left-2                          
+                    object-cover                    
+                    w-16                            
+                    rounded-full                    
+                    h-16                            
+                    fixed                           
+                    z-10                            
+                    outline-offset-2                
+                "
+            />
+            <div
+                className="
+                    bg-page-primaryColor
+                    flex 
+                    flex-col
+                    lg:flex-row
+                    h-screen
+                    items-center
+                    justify-center
+                    lg:justify-between
+                    tracking-wider
+                    content-center
+                "
+            >
+                <div
+                    className="
+                        flex
+                        my-auto
+                        flex-col
+                        gap-2
+                        self-center
+                        lg:ml-12
+                        space-y-2
+                        mx-2
+                    "
+                >
+                    <h1
+                        className="
+                            text-3xl
+                            md:text-5xl
+                            font-bold
+                            text-center
+                            lg:text-left
+                        "
+                    >
+                        Test Your Knowledge.
+                        <br />
+                        Have Fun !!
+                    </h1>
+                    <p
+                        className="
+                            font-light
+                            tracking-widest
+                            text-xl
+                            text-center
+                            lg:text-left
+                            md:text-3xl
+                        "
+                    >
+                        Take quizzes on various topics,
+                        <br />
+                        challenge yourself with timed tests,
+                        <br />
+                        and track your progress.
+                    </p>
+                    <div
+                        className="
+                            flex 
+                            flex-row 
+                            justify-center
+                            lg:justify-between
+                            flex-wrap
+                            gap-4
+                        "
+                    >
+                        <Button
+                            onClick={() => navigate('/quiz')}
+                            className="
+                                bg-customGreen
+                                text-white
+                                font-bolder
+                                text-2xl
+                                hover:bg-button-green
+                                md:flex-grow
+                                xl:flex-grow-0
+                                rounded-none
+                                p-8
+                                hover:bg-customGreen-light1
+                            "
+                        >
+                            Start a Simple Quiz
+                        </Button>
+                        <Button
+                            onClick={() => navigate('/quiz/test')}
+                            className="
+                                bg-customGreen
+                                text-white
+                                font-bolder
+                                hover:bg-button-green
+                                text-2xl
+                                md:flex-grow
+                                xl:flex-grow-0
+                                p-8
+                                rounded-none
+                                hover:bg-customGreen-light1
+                            "
+                        >
+                            Take a Quiz Test
+                        </Button>
+                    </div>
+                </div>
+                <div
+                    className="
+                        hidden
+                        lg:flex
+                        basis-1/2
+                        h-full
+                        justify-center
+                        items-center
+                    "
+                >
+                    <div
+                        className="
+                            w-2/3
+                            text-xl
+                            text-center
+                        "
+                    >
+                        <img
+                            src={McqImage}
+                            className="
+                                object-cover
+                                outline
+                                outline-gray-900
+                                outline-offset-2
+                            "
+                        />
+                    </div>
+                </div>
+            </div >
+        </div>
     )
 }
 
