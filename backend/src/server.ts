56b844e11a767f1b8b9a131c2f6f028ce727a00f
HarudaySharma <harudayvasist@gmail.com>
2024-06-17 15:34:46 +0530

    fix: flag images serving

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
-import express, { Application } from 'express';
-import {config} from 'dotenv';
+import express, { Application, Response } from 'express';
+import { config } from 'dotenv';
 import cookieParser from 'cookie-parser'
 
+import flagRoutes from './routes/flags.route.js'
 import testRoutes from './routes/test.route.js'
 import quizRoutes from './routes/quiz.route.js'
 
@@ -9,21 +10,23 @@ import { errorMiddleware } from './utils/errorHandler.js';
 
 config();
 
-
 const app: Application = express();
 
 const PORT: string = process.env.PORT || "3000";
 
-app.use(express.static('files/images'));
-
-app.listen(PORT, () => { 
-    console.log(`server listening on http://localhost:${PORT}`);
-})
-
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/quiz/test' , testRoutes);
-app.use('/api/quiz' , quizRoutes);
+app.listen(PORT, () => {
+    console.log(`server listening on http://localhost:${PORT}`);
+})
+
+app.get("/", (_, res: Response) => {
+    res.json({ status: "server running" });
+})
+
+app.use('/', flagRoutes);
+app.use('/api/quiz/test', testRoutes);
+app.use('/api/quiz', quizRoutes);
 
 app.use(errorMiddleware);
